d1b981cb777abc6e78a62a777707e31b41b88a36
Earl Jay Caoile <unfie555@gmail.com>
2020-06-18 00:26:41 -0700

    fixed content spacing issues yet again...and used variables to try to maintain consistency between ratios

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 /**
- * Parent component that shows history of API requests
+ * Simple footer content that shows up on every page. Contains name and shameless social media plugs
  *
  * @component
  * @example
  * return (
- *   <History />
+ *   <Footer />
  * )
  */
 function Footer() {
