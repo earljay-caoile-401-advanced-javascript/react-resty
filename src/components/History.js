cec38f06e8abe359192695459eff189adf8e893e
Earl Jay Caoile <unfie555@gmail.com>
2020-06-19 22:01:02 -0700

    checkpoint: refactored inline history comp to have a submit button instead of just clicking

--- src/components/History.js
+++ src/components/History.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import MiniHistory from './MiniHistory';
+// import MiniHistory from './MiniHistory';
+// import { Link } from 'react-router-dom';
 
 /**
  * Parent component that shows history of API requests
@@ -14,24 +15,64 @@ class History extends React.Component {
   constructor(props) {
     super(props);
     console.log('Do we have props?', props);
+    this.state = {
+      historyArr: [],
+      currDetails: {},
+    };
+
+    for (let i = 0; i < props.fetchHistory.length; i++) {
+      const currHistory = props.fetchHistory[i];
+      this.state.historyArr.push(
+        <li key={i} value={i} onClick={this.getFetchDetails(i)}>
+          {currHistory.reqType} {currHistory.url}
+        </li>
+      );
+    }
+
+    this.setState({ currDetails: props.fetchHistory[0] });
   }
 
-  fetchPrevReq(index) {
+  getFetchDetails(index) {
     console.log('Are we getting index here?', index);
+    this.setState({ currDetails: this.props.fetchHistory[index] });
   }
 
   render() {
     return (
-      <div className="content flex-row">
+      <div className="content margin-1">
         <h2>API Fetch History</h2>
-        <div className="big-history">
-          <MiniHistory
-            fetchHistory={this.props.fetchHistory || []}
-            onHistClick={this.fetchPrevReq.bind(this)}
-          />
+        <div className="flex-row">
+          <div className="big-history">
+            <div className="mini-history">
+              <h3>History</h3>
+              <ul className="history-list">{this.state.historyArr}</ul>
+            </div>
+            <div className="fetch-details">
+              <h3>Dem Details</h3>
+              <div>{this.currDetails}</div>
+            </div>
+          </div>
         </div>
       </div>
     );
+
+    // return (
+    //   <div className="content margin-1">
+    //     <h2>API Fetch History</h2>
+    //     <div className="flex-row">
+    //       <div className="big-history">
+    //         <div className="mini-history">
+    //           <h3>History</h3>
+    //           <ul className="history-list">{this.state.historyArr}</ul>
+    //         </div>
+    //         <div className="fetch-details">
+    //           <h3>Dem Details</h3>
+    //           <div>{this.currDetails}</div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // );
   }
 }
 
--- src/components/MiniHistory.js
+++ src/components/MiniHistory.js
@@ -1,25 +1,47 @@
 import React from 'react';
 
 function MiniHistory(props) {
-  const histotryArr = [];
+  const historyArr = [];
+  let selectedIndex = null;
 
   for (let i = 0; i < props.fetchHistory.length; i++) {
     const currHistory = props.fetchHistory[i];
-    histotryArr.push(
-      <li key={i} value={i} onClick={handleHistClick}>
+    historyArr.push(
+      <li key={i} value={i} onClick={updateActiveIndex}>
         {currHistory.reqType} {currHistory.url}
       </li>
     );
   }
 
+  function updateActiveIndex(e) {
+    const prevSelected = document.querySelector('.selected-btn');
+    if (prevSelected) {
+      prevSelected.classList.remove('selected-btn');
+    }
+    e.target.classList.add('selected-btn');
+    console.log('What is e.target?', e.target);
+    selectedIndex = e.target.value;
+    console.log('selected index is now this:', selectedIndex);
+  }
+
   function handleHistClick(e) {
-    props.onHistClick(e.target.value);
+    console.log('What is e?', e);
+    if (selectedIndex !== null) {
+      props.onHistClick(selectedIndex);
+      const prevSelected = document.querySelector('.selected-btn');
+      if (prevSelected) {
+        prevSelected.classList.remove('selected-btn');
+      }
+    }
   }
 
   return (
     <div className="mini-history">
-      <h3>History</h3>
-      <ul className="history-list">{histotryArr}</ul>
+      <h3>History (click query to re-fetch)</h3>
+      <ul className="history-list">{historyArr}</ul>
+      <button id="submit-btn" value={selectedIndex} onClick={handleHistClick}>
+        Submit
+      </button>
     </div>
   );
 }
